d70ac941cb2a81b469a7c6f910961049a1fb4c12
Aidan Lister <aidan@php.net>
2009-06-06 15:04:02 +0000

    Finish debugging tool and modularise event parsing. New event parser does much better job with dates.

--- theme/agenda-debug.tpl.php
+++ theme/agenda-debug.tpl.php
@@ -1,11 +1,34 @@
+<style type="text/css">
+#agenda-debug ul li pre {
+  max-height: 150px;
+  border: 1px solid #ccc;
+  overflow: auto;
+  font-size: 80%;
+  line-height: 100%;
+  font-family: monospace;
+}
+
+#agenda-debug em {
+  background-color: #f1f1f1;
+  }
+
+#agenda-debug {
+  border: 1px solid #ccc;
+  padding-right: 1em;
+  font-size: 95%;
+  line-height: 130%;
+  font-family: monospace;
+}
+</style>
+
 <?php echo $form; ?>
 
+<?php if (!empty($output)): ?>
 <div id="agenda-debug">
-  <?php if (!empty($output)): ?>
-    <ul>
-    <?php foreach ($output as $line): ?>
-      <li><?php echo $line; ?></li>
-    <?php endforeach; ?>
-    </ul>
-  <?php endif; ?>
+  <ul>
+  <?php foreach ($output as $line): ?>
+    <li><?php echo $line; ?></li>
+  <?php endforeach; ?>
+  </ul>
 </div>
+<?php endif; ?>

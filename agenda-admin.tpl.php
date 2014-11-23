60f3b224a71744b6b4e75ae030687513747d583f
oenie <oenie@1446068.no-reply.drupal.org>
2014-11-23 14:47:43 -0600

    Issue #2376795 by gooddesignusa, oenie: Separated Google API settings to another tab; cleaned whitespace.

--- agenda-admin.tpl.php
+++ agenda-admin.tpl.php
@@ -10,5 +10,3 @@
 <?php else: ?>
   <p>You have not created any blocks yet.</p>
 <?php endif; ?>
-<p>&raquo; <?php echo l('Create a new block', 'admin/config/services/agenda/0/configure'); ?>.</p>
-<p>&raquo; <?php echo l('Set Google API Key', 'admin/config/services/agenda/googleapi'); ?>.</p>
